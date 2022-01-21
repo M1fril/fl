cc1a41badf46e9d8d2e6ffa726ddb68b8f14db69
Your Name <you@example.com>
2022-01-21 13:28:48 +0200

    code update

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -23,7 +23,6 @@ class User(db.Model):
     email = db.Column(db.String(128), unique=True, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
 
-
     def __init__(self, email):
         self.email = email
 
